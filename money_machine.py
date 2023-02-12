345f4cc522f2151b1d35b0ee14f295387a85659d
Suryadhanasekar <suryad6201@gmail.com>
2023-02-12 12:51:10 +0530

    coffee machine initial commit

---
+++ money_machine.py
@@ -0,0 +1,37 @@
+class MoneyMachine:
+
+    CURRENCY = "$"
+
+    COIN_VALUES = {
+        "quarters": 0.25,
+        "dimes": 0.10,
+        "nickles": 0.05,
+        "pennies": 0.01
+    }
+
+    def __init__(self):
+        self.profit = 0
+        self.money_received = 0
+
+    def report(self):
+        print(f"Money: {self.CURRENCY}{self.profit}")
+
+    def process_coins(self):
+        print("Please insert coins.\n"
+              "Note: 1$ = 100 cents -- 1 quarter: 25cents | 1 dime: 10cents | 1 nickel: 5cents | 1 penny: 1 cent")
+        for coin in self.COIN_VALUES:
+            self.money_received += int(input(f"How many {coin}?: ")) * self.COIN_VALUES[coin]
+        return self.money_received
+
+    def make_payment(self, cost):
+        self.process_coins()
+        if self.money_received >= cost:
+            change = round(self.money_received - cost, 2)
+            print(f"Here is {self.CURRENCY}{change} in change.")
+            self.profit += cost
+            self.money_received = 0
+            return True
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+            self.money_received = 0
+            return False
